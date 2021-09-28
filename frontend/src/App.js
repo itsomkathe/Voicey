9b231eee987e2af42a8354d56452318d541bbf66
Om Kathe <omkathe26@gmail.com>
2021-09-28 17:27:13 +0530

    feat: added-protected-route

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,29 +1,49 @@
 import "./App.css";
-import { BrowserRouter, Switch, Route } from "react-router-dom";
+import { BrowserRouter, Switch, Route, Redirect } from "react-router-dom";
 import Navbar from "./Components/Common/Navbar/Navbar";
 import Intro from "./Screens/Intro/Intro";
-import Signup from "./Screens/Signup/Signup";
 import Signin from "./Screens/Signin/Signin";
+import Authentication from "./Screens/Authentication/Authentication";
 
+const isAuth = true;
 function App() {
     return (
         <>
-        <BrowserRouter>
-            <Navbar/>
-            <Switch>
-                <Route path = "/" exact = {true}>
-                    <Intro/>
-                </Route>
-                <Route path = "/signup" exact = {true}>
-                    <Signup/>
-                </Route>
-                <Route path = "/signin" exact = {true}>
-                    <Signin/>
-                </Route>
-            </Switch>
-        </BrowserRouter>
+            <BrowserRouter>
+                <Navbar />
+                <Switch>
+                    <Route path="/" exact={true}>
+                        <Intro />
+                    </Route>
+                    <GuestRoute path="/authentication" exact={true}>
+                        <Authentication />
+                    </GuestRoute>
+                    <Route path="/signin" exact={true}>
+                        <Signin />
+                    </Route>
+                </Switch>
+            </BrowserRouter>
         </>
     );
 }
 
+const GuestRoute = ({ children, ...rest }) => {
+    return (
+        <Route
+            {...rest}
+            render={({ location }) => {
+                return isAuth ? (
+                    <Redirect
+                        to={{
+                            pathname: "/rooms",
+                            state: { from: location },
+                        }}
+                    />
+                ) : (
+                    children
+                );
+            }}
+        ></Route>
+    );
+};
 export default App;
--- frontend/src/Screens/Authentication/Authentication.jsx
+++ frontend/src/Screens/Authentication/Authentication.jsx
@@ -1,23 +1,19 @@
 import React, {useState} from "react";
-import EnterName from "../Steps/EnterName/EnterName";
 import EnterOTP from "../Steps/EnterOTP/EnterOTP";
 import EnterPassword from "../Steps/EnterPassword/EnterPassword";
 import EnterPhoneMail from "../Steps/EnterPhoneMail/EnterPhoneMail";
-import EnterPic from "../Steps/EnterPic/EnterPic";
 import EnterUsername from "../Steps/EnterUsername/EnterUsername";
 
-const signUpSteps = {
+const authSteps = {
     1: EnterPhoneMail,
     2: EnterOTP,
     3: EnterUsername,
     4: EnterPassword,
-    5: EnterName,
-    6: EnterPic
 }
 
-export default function Signup(){
+export default function Authentication(){
     const[step, setStep] = useState(1);
-    const Comp = signUpSteps[step];
+    const Comp = authSteps[step];
     const nextStep = ()=>{
         setStep(step+1);
     }
