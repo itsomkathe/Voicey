a856175a74461074c9479e543e2d6aab3e6b3f4e
Om Kathe <omkathe26@gmail.com>
2021-10-08 18:58:51 +0530

    feat: added-sms-sender-and-verify

--- backend/services/otpService.js
+++ backend/services/otpService.js
@@ -1,20 +1,38 @@
 const otpGenerator = require('otp-generator');
+const hashingService = require('./hashingService');
+const smsID = process.env.SMS_ID;
+const smsAuth = process.env.SMS_AUTH;
+const smsSender = process.env.SMS_SENDER;
+
+const twilio = require('twilio')(smsID, smsAuth, {
+    lazyLoading: true
+});
+
 class OTPService{
     async generateOTP(){
         const otp = otpGenerator.generate(4, {digits:true, upperCase: false, specialChars: false, alphabets:false});
         return otp;
     }
 
-    sendAsText(){
-
+    async sendAsText(phoneNumber, otp){
+        return await twilio.messages.create({
+            to: phoneNumber,
+            from: smsSender,
+            body: `Your voicey OTP is ${otp}`
+        });
     }
     
     sendAsMail(){
 
     }
 
-    verify(){
-
+    async verify(hashedOTP, data){
+        const toVerify = await hashingService.hashOTP(data);
+        if(hashedOTP === toVerify){
+            return true;
+        }else{
+            return false;
+        }
     }
 }
 
