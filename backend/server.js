456748f296fd254f9c82efc62beafaf4355f1377
Om Kathe <omkathe26@gmail.com>
2021-10-08 19:01:23 +0530

    feat: connection-to-db

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,26 @@
 require('dotenv').config();
 const express = require('express');
+const mongoose = require('mongoose');
 const app = express();
 const PORT = process.env.PORT || 5000;
+const MONGO_URL = process.env.MONGO_URL;
 const router = require('./routes');
 app.use(express.json());
 app.use(router);
+
+mongoose.connect(MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+});
+
+mongoose.connection.on('connected', () => {
+    console.log('Connected');
+});
+
+mongoose.connection.on('error', (err) => {
+    console.log('Error', err);
+});
 
 app.get('/',(req,res,next)=>{
     res.send('Welcome to voicey');
