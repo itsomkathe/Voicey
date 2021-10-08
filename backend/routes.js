78bb7aa1b3e773ff317b3682b01bf46afc69a4da
Om Kathe <omkathe26@gmail.com>
2021-10-08 19:00:23 +0530

    refactor: verify-otp-route

--- backend/routes.js
+++ backend/routes.js
@@ -4,7 +4,7 @@ const AuthController = require('./controllers/authController');
 
 router.post('/api/send-otp', AuthController.sendOTP);
 
-
+router.post('/api/verify-otp', AuthController.verifyOTP);
 
 module.exports = router;
 
