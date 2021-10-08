c17f9e68c9164e328a052f9c58ea3d1b9a2539f5
Om Kathe <omkathe26@gmail.com>
2021-10-08 19:00:03 +0530

    feat: send-otp-controller

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -1,15 +1,39 @@
-const otpService = require('../services/otpService');
-const hashingService = require('../services/hashingService');
-class AuthController{
-    async sendOTP(req,res){
-        const {number} = req.body;
-        if(!number) res.status(400).send('Add phone number');
+const otpService = require("../services/otpService");
+const hashingService = require("../services/hashingService");
+
+class AuthController {
+    async sendOTP(req, res) {
+        const { number } = req.body;
+        if (!number) res.status(400).send("Add phone number");
         const otp = await otpService.generateOTP();
-        const time = 1000*60*5;
+        const time = 1000 * 60 * 5;
         const expireAt = Date.now() + time;
         const data = `${number}#${otp}#${expireAt}`;
-        const hash  = await hashingService.hashOTP(data);
-        return res.json({data: hash});
+        const hash = await hashingService.hashOTP(data);
+
+        try {
+            await otpService.sendAsText(number, otp);
+            return res.json({ hash: `${hash}#${expireAt}`, number: number });
+        } catch (err) {
+            return res.status(500).json({ error: err });
+        }
+    }
+
+    async verifyOTP(req, res){
+        const{number, otp, hash} = req.body;
+        if(!otp || !number || !hash){
+            return res.staus(400).json({message: "All fields are required"});
+        }
+        const [hashedOTP, expireAt] = hash.split("#");
+        if(Date.now() > expireAt){
+            return res.staus(400).json({message: "OTP Expired"});
+        }
+
+        const data = `${number}#${otp}#${expireAt}`;
+        const isValid = await otpService.verify(hashedOTP, data);
+        if(!isValid){
+            return res.staus(400).json({message: "OTP Expired"});
+        }
     }
 }
 
