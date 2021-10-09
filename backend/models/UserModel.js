55db2f7fc3bfe4e1b4938186592b96272b8155bf
Om Kathe <omkathe26@gmail.com>
2021-10-09 15:21:20 +0530

    feat: user-model

---
+++ backend/models/UserModel.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema({
+    username: {
+        type: String
+    },
+    name:{
+        type: String
+    },
+    phone: {
+        type: String
+    },
+    username: {
+        type: String
+    }
+},{
+    timestamps : true
+});
+
+module.exports = mongoose.model('User',userSchema, users);
