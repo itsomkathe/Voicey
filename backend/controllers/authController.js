21e7c28140de80ac81bc072d79ddd6b8b42c5f83
Om Kathe <omkathe26@gmail.com>
2021-10-10 18:24:28 +0530

    fix: auth-token

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -1,11 +1,11 @@
 const otpService = require('../services/otpService');
 const hashingService = require('../services/hashingService');
 const userService = require('../services/userService');
-
+const tokenService = require('../services/tokenService');
 class AuthController {
     async sendOTP(req, res) {
         const { number } = req.body;
-        if (!number) res.status(400).send("Add phone number");
+        if (!number) return res.status(400).send("Add phone number");
         const otp = await otpService.generateOTP();
         const time = 1000 * 60 * 5;
         const expireAt = Date.now() + time;
@@ -14,7 +14,7 @@ class AuthController {
 
         try {
             await otpService.sendAsText(number, otp);
-            return res.json({ hash: `${hash}#${expireAt}`, number: number });
+            res.json({ hash: `${hash}#${expireAt}`, number: number });
         } catch (err) {
             return res.status(500).json({ error: err });
         }
@@ -33,24 +33,32 @@ class AuthController {
         const data = `${number}#${otp}#${expireAt}`;
         const isValid = await otpService.verify(hashedOTP, data);
         if(!isValid){
-            return res.staus(400).json({message: "OTP Expired"});
+            return res.staus(400).json({message: "Incorrect OTP"});
         }
 
         let user;
         try{
             user = await userService.findUser({phone: number});
             if(user){
-                return res.status(400).json({message: "user with given number already exists"})
-            }
-            try{
-                await userService.createUser({phone: number})
-            }catch(err){
-                console.log(err);
-                return res.status(500).json({message: "Internal server error, could not create account"})
+                return res.status(400).json({message: "User with given number already exists"});
             }
         }catch(err){
             console.log(err);
+            return res.status(500).json({message: "Internal server error"});
         }
+
+        //const {accessToken, refreshToken} = tokenService.createToken({id: user._id});
+        const authToken = tokenService.createAuthToken({phone: number});
+        res.cookie('authtoken', authToken, {
+            maxAge: 1000*60*60,
+            httpOnly: true
+        });
+        res.json({message: "Cookie Sent"})
+        /*  res.cookie('refreshtoken', refreshToken, {
+            maxAge: 1000*60*60*24*30,
+            httpOnly: true
+        });
+        res.json({accessToken}) */
     }
 }
 
