ac29c37bf1e4a3a7787eef5e825debab1209655f
Om Kathe <omkathe26@gmail.com>
2021-10-22 16:53:27 +0530

    fix: alignment

--- frontend/src/Screens/Steps/EnterName/EnterName.jsx
+++ frontend/src/Screens/Steps/EnterName/EnterName.jsx
@@ -2,13 +2,19 @@ import React from "react";
 import style from './EnterName.module.css'
 import Button from "../../../Components/Common/Button/Button";
 import Card from "../../../Components/Common/Card/Card";
+import Input from "../../../Components/Common/Input/Input";
 
 export default function EnterName({onclick}){
     return(
         <>
-            <Card title = "enter profile name" icon = "nerd.svg">
-                <Button onClick = {onclick} text = "Continue"></Button>
-            </Card>
+            <div className={style.cardWrapper}>
+                <Card title = "enter profile name" icon = "nerd.svg">
+                    <div className = {style.inputWrapper}>
+                        <Input placeholder = "Enter your name"/>
+                    </div>
+                    <Button onClick = {onclick} text = "Continue"></Button>
+                </Card>
+            </div>  
         </>
     )
 }
