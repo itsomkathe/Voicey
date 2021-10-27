aaac3e0db11a1514b136d1ff4d8c134d504c612b
Om Kathe <omkathe26@gmail.com>
2021-10-27 22:15:35 +0530

    feat: add-photo

--- frontend/src/Screens/Steps/AddPhoto/AddPhoto.jsx
+++ frontend/src/Screens/Steps/AddPhoto/AddPhoto.jsx
@@ -1,20 +1,94 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useRef } from "react";
 import style from "./AddPhoto.module.css";
 import Button from "../../../Components/Common/Button/Button";
 import Card from "../../../Components/Common/Card/Card";
 import { useSelector } from "react-redux";
 import { Link } from "react-router-dom";
 
-export default function AddPhoto(){
-    const globalName = "Om"
-    return(
-    <>
-        <Card  title= {`alright ${globalName}, add a photo`} icon="camera.png">
-            <Button
-                text="Set Picture"
-            />
-            <span className = {style.message}>you can do this step later, <Link>Skip</Link></span>
-        </Card>
-    </>
-    )
+export default function AddPhoto() {
+    const [image, setImage] = useState(null);
+    const [didMount, setDidMount] = useState(false);
+    const [picID, setPicID] = useState(1);
+    
+    const globalName = "Om";
+    const inputRef = useRef();
+    useEffect(() => {
+        setDidMount(true);
+        document.title = "Profile Picture";
+        return () => {
+            setDidMount(false);
+        };
+    }, []);
+
+    const handleUploadClick = (e) => {
+        e.preventDefault();
+        inputRef.current.click();
+    };
+
+    const displayImage = (event) => {
+        const file = event.target.files[0];
+        if (file && file.type.substr(0, 5) === "image") {
+            const reader = new FileReader();
+            reader.onloadend = () => {
+                if (didMount) {
+                    setImage(reader.result);
+                }
+            };
+            reader.readAsDataURL(file);
+        } else {
+            if (didMount) {
+                setImage(null);
+            }
+        }
+    };
+
+    const newPicID = () => {
+        setPicID((picID%5) + 1);
+    };
+
+    return (
+        <>
+            <Card
+                title={`alright ${globalName}, add a photo`}
+                icon="camera.png"
+            >
+                <div className={style.picWrapper}>
+                    {image ? (
+                        <img src={image} alt="pic"></img>
+                    ) : (
+                        <img
+                            src={`/Resources/Avatars/${picID}.jpg`}
+                            alt="pic"
+                        ></img>
+                    )}
+                </div>
+
+                <span onClick={newPicID} id={style.generate}>
+                    Generate an avatar
+                </span>
+
+                <div className={style.buttonWrapper}>
+                    <Button
+                        text="Upload"
+                        onClick={handleUploadClick}
+                        disabled={false}
+                        color="WHITE"
+                    />
+                    <Button text="Save Picture" icon={true} />
+                </div>
+
+                <input
+                    type="file"
+                    className={style.fileInput}
+                    ref={inputRef}
+                    accept="image/*"
+                    onChange={displayImage}
+                ></input>
+
+                <span className={style.message}>
+                    you can do this step later, <Link>Skip</Link>
+                </span>
+            </Card>
+        </>
+    );
 }
