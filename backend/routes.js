a1f1938f5e1dd7790cee2c7828346fa7cf33d117
Om Kathe <omkathe26@gmail.com>
2021-10-30 22:18:37 +0530

    refactor: create-account-route

--- backend/routes.js
+++ backend/routes.js
@@ -1,10 +1,13 @@
 const express = require('express')
 const router = express.Router();
 const AuthController = require('./controllers/authController');
+const AccountController = require('./controllers/accountController');
 
 router.post('/api/send-otp', AuthController.sendOTP);
 
 router.post('/api/verify-otp', AuthController.verifyOTP);
+
+router.post('/api/createaccount', AccountController.createAccount);
 
 module.exports = router;
 
