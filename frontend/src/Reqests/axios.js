d95250ca662708a84c895924a16c07fc64433f5d
Om Kathe <omkathe26@gmail.com>
2021-11-11 16:22:51 +0530

    refactor: create-account-endpoint

--- frontend/src/Reqests/axios.js
+++ frontend/src/Reqests/axios.js
@@ -11,5 +11,5 @@ const api = axios.create({
 
 export const sendOTP = (data)=> api.post('/api/send-otp', data);
 export const verifyOTP = (data)=> api.post('/api/verify-otp', data);
-
+export const createAccount = (data)=>api.post('/api/createaccount', data);
 export default api;
