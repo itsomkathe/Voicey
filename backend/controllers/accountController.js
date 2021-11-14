4e27b2c649e6a7fc4988ef0944746dd16fcff6ec
Om Kathe <omkathe26@gmail.com>
2021-11-14 17:58:44 +0530

    feat: create-account

--- backend/controllers/accountController.js
+++ backend/controllers/accountController.js
@@ -1,6 +1,21 @@
+const UserService = require('../services/userService');
+const HashingService = require('../services/hashingService');
+
 class AccountController{
     async createAccount(req, res){
-        
+        const {phone, username, password, name} = req.body;
+        try{
+            const existing = await UserService.findUser({$or:[{phone},{username}]});
+            console.log(existing);
+            if(existing){
+                return res.json({error: "Account already exists"});
+            }
+            const hashedPassword = await HashingService.hashPassword(password);
+            const userData = await UserService.createUser({phone, username, password:hashedPassword, name});
+            res.json(userData);
+        }catch(err){
+            res.json({error: "Internal server error"});
+        }
     }
 }
 
