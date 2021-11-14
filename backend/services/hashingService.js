903d9638f4f090cf727710bc78b5b37d37901d64
Om Kathe <omkathe26@gmail.com>
2021-11-14 13:07:54 +0530

    feat: password-hashing

--- backend/services/hashingService.js
+++ backend/services/hashingService.js
@@ -4,6 +4,11 @@ class HashingService{
         const hashedOTP = await crypto.createHmac('sha512', process.env.HASH).update(data).digest('hex');
         return hashedOTP;
     }
+
+    async hashPassword(password){
+        const hashedPassword = await crypto.createHmac('sha256', process.env.HASH).update(password).digest('hex');
+        return hashedPassword;
+    }
 }
 
 module.exports =  new HashingService();
