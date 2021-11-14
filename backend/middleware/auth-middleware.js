8acebf582c1bdaeefdf0be3021ed2b45b9aedaef
Om Kathe <omkathe26@gmail.com>
2021-11-14 13:08:30 +0530

    fix: next-middleware

--- backend/middleware/auth-middleware.js
+++ backend/middleware/auth-middleware.js
@@ -2,16 +2,16 @@ const tokenService = require('../services/tokenService');
 module.exports = async (req, res, next)=>{
     try{
         const { verificationToken } = req.cookies;
-        console.log(verificationToken);
         if(!verificationToken){
+
             throw new Error();
         }
         const data = await tokenService.checkVerificationToken(verificationToken);
-        console.log(data);
-        if(data){
+        if(!data){
             throw new Error();
         }
         req.user = data;
+        next();
     }catch(err){
         res.json({error: "Verification failed"})
     }
