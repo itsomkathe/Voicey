a13b706eb2d09a895c55825403e39ee57d48489d
Om Kathe <omkathe26@gmail.com>
2021-11-24 20:01:38 +0530

    fix: error-message-json

--- backend/middleware/auth-middleware.js
+++ backend/middleware/auth-middleware.js
@@ -3,16 +3,19 @@ module.exports = async (req, res, next)=>{
     try{
         const { verificationToken } = req.cookies;
         if(!verificationToken){
-
-            throw new Error();
+            throw new Error("Session Expired");
         }
         const data = await tokenService.checkVerificationToken(verificationToken);
         if(!data){
-            throw new Error();
+            throw new Error("Failed to verify");
+        }
+        
+        if(data.exp < Date.now()){
+            throw new Error("Session Expired");
         }
         req.user = data;
         next();
     }catch(err){
-        res.json({error: "Verification failed"})
+        res.status(401).json({error: err.message})
     }
 }
