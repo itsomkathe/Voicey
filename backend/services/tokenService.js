722fe893c690f7bba60407e2ad93435f7708d9fa
Om Kathe <omkathe26@gmail.com>
2021-11-26 15:56:17 +0530

    refactor: access-token

--- backend/services/tokenService.js
+++ backend/services/tokenService.js
@@ -1,18 +1,9 @@
 const jwt = require('jsonwebtoken');
 class TokenService{
-    createToken(payload){
-        const JWT_ACCESS_SECRET = process.env.JWT_ACCESS_SECRET;
-        const accessToken = jwt.sign(payload, JWT_ACCESS_SECRET, {
-            expiresIn : '1h'
-        });
-    
-        return {accessToken};
-    }
-
     createVerificationToken(payload){
         const JWT_AUTH_SECRET = process.env.JWT_ACCESS_SECRET;
         const verificationToken = jwt.sign(payload, JWT_AUTH_SECRET, {
-            expiresIn: '7d'
+            expiresIn: '1h'
         });
         return verificationToken;
     }
@@ -23,8 +14,8 @@ class TokenService{
     }
 
     createAccessToken(payload){
-        const JWT_AUTH_SECRET = process.env.JWT_ACCESS_SECRET;
-        const accessToken = jwt.sign(payload, JWT_AUTH_SECRET, {
+        const JWT_ACCESS_SECRET = process.env.JWT_ACCESS_SECRET;
+        const accessToken = jwt.sign(payload, JWT_ACCESS_SECRET, {
             expiresIn: '7d'
         });
         return accessToken;
