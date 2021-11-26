65fa3bc7a9ba22506068cf9f16756cbf0ba415ad
Om Kathe <omkathe26@gmail.com>
2021-11-26 15:55:19 +0530

    feat: access-token-cookie

--- backend/controllers/accountController.js
+++ backend/controllers/accountController.js
@@ -1,21 +1,30 @@
 const UserService = require('../services/userService');
 const HashingService = require('../services/hashingService');
-
+const TokenService = require('../services/tokenService');
 class AccountController{
     async createAccount(req, res){
         const {phone, username, password, name} = req.body;
         try{
             const existing = await UserService.findUser({$or:[{phone},{username}]});
-            console.log(existing);
             if(existing){
                 return res.json({error: "Account already exists"});
             }
             const hashedPassword = await HashingService.hashPassword(password);
             const userData = await UserService.createUser({phone, username, password:hashedPassword, name});
+            const accessToken = await TokenService.createAccessToken({phone, username});
+            res.cookie('accessToken', accessToken, {
+                maxAge: 1000*60*60*24*7,
+                httpOnly: true
+            });
             res.json(userData);
+            
         }catch(err){
             res.json({error: "Internal server error"});
         }
+    }
+
+    async getProfile(req, res){
+
     }
 }
 
