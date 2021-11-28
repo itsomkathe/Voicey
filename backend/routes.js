d3ee6c3514dc368dc6c019020111620142e8e7d1
Om Kathe <omkathe26@gmail.com>
2021-11-28 13:40:12 +0530

    feat: refresh-route

--- backend/routes.js
+++ backend/routes.js
@@ -3,12 +3,15 @@ const router = express.Router();
 const AuthController = require('./controllers/authController');
 const AccountController = require('./controllers/accountController');
 const AuthMiddleware = require('./middleware/auth-middleware');
+const AccessMiddleware = require('./middleware/access-middleware');
 
 router.post('/api/send-otp', AuthController.sendOTP);
 
 router.post('/api/verify-otp', AuthController.verifyOTP);
 
 router.post('/api/createaccount', AuthMiddleware, AccountController.createAccount);
+
+router.get('/api/refresh', AccessMiddleware, AccountController.getProfile);
 
 module.exports = router;
 
