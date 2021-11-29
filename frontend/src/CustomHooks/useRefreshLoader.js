6285da7888ade5eb4055c2cb9b36bde234a184b5
Om Kathe <omkathe26@gmail.com>
2021-11-29 18:09:02 +0530

    feat: refresh-data-loader

--- frontend/src/CustomHooks/useRefreshLoader.js
+++ frontend/src/CustomHooks/useRefreshLoader.js
@@ -1,10 +1,27 @@
 import {useState, useEffect} from 'react';
 import axios from 'axios';
+import { useDispatch } from 'react-redux';
+import { setProfile, setIsAuth } from '../Store/ProfileSlice';
+
 export function useRefreshLoader(){
     const[loading, setLoading] = useState(true);
+    const dispatch = useDispatch();
     useEffect(()=>{
         (async ()=>{
-            await axios.get('/')
+            try{
+                const {data} = await axios.get(`${process.env.REACT_APP_BACKEND_URL}/api/refresh`, 
+                {
+                    withCredentials: true
+                })
+                const profile = {...data, isAuth: true};
+                await dispatch(setProfile(profile));
+                setLoading(false);
+            }catch(err){
+                await dispatch(setIsAuth({isAuth: false}));
+                setLoading(false)
+            }
         })();
     }, [])
+
+    return { loading }
 }
