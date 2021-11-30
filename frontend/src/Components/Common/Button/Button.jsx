2464427d1165839c33863b2e5b051ebcf9ec7058
Om Kathe <omkathe26@gmail.com>
2021-11-30 16:11:58 +0530

    feat: button-type

--- frontend/src/Components/Common/Button/Button.jsx
+++ frontend/src/Components/Common/Button/Button.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import style from "./Button.module.css";
-export default function Button({ text, onClick, disabled, color, icon }) {
+export default function Button({ text, onClick, disabled, color, icon, type }) {
     let id = style.buttonBlue;
     if(color === "GREEN"){
         id = style.buttonGreen;
@@ -17,6 +17,7 @@ export default function Button({ text, onClick, disabled, color, icon }) {
                 className = {style.button}
                 disabled = {disabled}
                 id = {id}
+                type = {type ? type : null}
             >
                 <span>{text}</span>
                 {icon ? <img src="/Resources/Icons/right-arrow.svg" alt="icon"></img> : null}
