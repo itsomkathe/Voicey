c82f75839a63a30f88777a889b6218a45a013255
Om Kathe <omkathe26@gmail.com>
2021-11-30 16:13:26 +0530

    feat: sign-in-form

--- frontend/src/Screens/Signin/Signin.jsx
+++ frontend/src/Screens/Signin/Signin.jsx
@@ -1,28 +1,89 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import style from "./Signin.module.css";
 import Button from "../../Components/Common/Button/Button";
 import Card from "../../Components/Common/Card/Card";
 import Input from "../../Components/Common/Input/Input";
+import { signIn } from "../../Reqests/axios";
+import { setProfile, setIsAuth } from "../../Store/ProfileSlice";
+import { useDispatch } from "react-redux";
+import { useHistory } from "react-router-dom";
 
-export default function Signin(){
-    
-    return(
+export default function Signin() {
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+    const [allow, setAllow] = useState(false);
+    const [error, setError] = useState('');
+    const dispatch = useDispatch();
+    const hist = useHistory();
+
+    useEffect(()=>{
+        if(username.length >= 3 && password.length >= 6){
+            setAllow(true);
+        }else{
+            setAllow(false);
+        }
+        
+    },[username, password])
+
+    function changeUsername(event) {
+        setUsername(event.target.value);
+    }
+
+    function changePassword(event) {
+        setPassword(event.target.value);
+    }
+
+    async function submit(event) {
+        event.preventDefault();
+        try{
+            const { data } = await signIn({
+                username, 
+                password
+            });
+            dispatch(setProfile(data));
+            dispatch(setIsAuth(true));
+            hist.push("/rooms");
+        }catch(err){
+            setError(err.response.data.error ? err.response.data.error : "Error Occured" );
+        }
+    }
+    return (
         <>
-            <div className = {style.cardWrapper}>
-                <Card title = "sign in" icon = "unlock.svg">
-                    <div className = {style.inputWrapper}>
-                        <Input placeholder = "username"/>
-                    </div>
-                    <div className = {style.inputWrapper}>
-                        <Input placeholder = "password"/>
-                    </div>
-                    <div className = {style.buttonWrapper}>
-                        <Button text = "Sign In"/>
-                    </div>
-                    <span className={style.message}>want to test this app? <span className = {style.clickHere}>Click Here</span></span>
+            <div className={style.cardWrapper}>
+                <Card title="sign in" icon="unlock.svg">
+                    <form className={style.form} onSubmit = {submit} >
+                        <div className={style.inputWrapper}>
+                            <Input
+                                type="text"
+                                value={username}
+                                placeholder="username"
+                                onchange={changeUsername}
+                            />
+                        </div>
+                        <div className={style.inputWrapper}>
+                            <Input
+                                type="password"
+                                value={password}
+                                placeholder="password"
+                                onchange={changePassword}
+                            />
+                        </div>
+                        {error ? <span className={style.warning}>{error}</span> : null}
+                        <div className={style.buttonWrapper}>
+                            <Button
+                                disabled={!allow}
+                                type="submit"
+                                text="Sign In"
+                                icon = {true}
+                            />
+                        </div>
+                    </form>
+                    <span className={style.message}>
+                        want to test this app?{" "}
+                        <span className={style.clickHere}>Click Here</span>
+                    </span>
                 </Card>
             </div>
-            
         </>
-    )
+    );
 }
