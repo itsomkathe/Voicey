97977dddb2b80352e240d69092ace216dd449127
Om Kathe <omkathe26@gmail.com>
2021-12-02 20:03:48 +0530

    feat: add-photo-api-call

--- frontend/src/Reqests/axios.js
+++ frontend/src/Reqests/axios.js
@@ -13,4 +13,5 @@ export const sendOTP = (data)=> api.post('/api/send-otp', data);
 export const verifyOTP = (data)=> api.post('/api/verify-otp', data);
 export const createAccount = (data)=> api.post('/api/createaccount', data);
 export const signIn = (data)=> api.post('/api/signin', data);
+export const addPhoto = (data)=> api.post('/api/addphoto', data);
 export default api;
