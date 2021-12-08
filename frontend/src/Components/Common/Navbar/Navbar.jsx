125f3e601afe92d59a0e1e3a8d103723c0f0b3a7
Om Kathe <omkathe26@gmail.com>
2021-12-08 19:05:39 +0530

    feat: nav-picture

--- frontend/src/Components/Common/Navbar/Navbar.jsx
+++ frontend/src/Components/Common/Navbar/Navbar.jsx
@@ -1,13 +1,34 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import style from "./Navbar.module.css";
+import { useSelector } from "react-redux";
+
 export default function Navbar(){
+    const { name, picture, isAuth } = useSelector((state)=>{
+        return state.profile;
+    });
     return(
     <>
         <nav className = {`${style.navbar} container`}>
             <Link to = '/'>
                 <img id = {`${style.logo}`} src = '/Resources/Icons/logo.png' alt = "logo"></img>
             </Link>
+            {
+                isAuth && 
+                <div className={style.navRight}>
+                <h3>{name}</h3>
+                    <Link to="/">
+                        <img
+                            className={style.picture}
+                            src={
+                                picture ? picture: `/Resources/Avatars/1.jpg`
+                            }
+                            alt="avatar"
+                        />
+                    </Link>
+                </div>
+            }
+            
         </nav>
     </>)
 }
