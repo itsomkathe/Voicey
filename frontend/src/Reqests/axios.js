96bcb637f7ae3d284fed894820c11f1c60105e3d
Om Kathe <omkathe26@gmail.com>
2021-12-16 18:02:51 +0530

    feat: get-rooms-function-endpoint

--- frontend/src/Reqests/axios.js
+++ frontend/src/Reqests/axios.js
@@ -14,4 +14,5 @@ export const verifyOTP = (data)=> api.post('/api/verify-otp', data);
 export const createAccount = (data)=> api.post('/api/createaccount', data);
 export const signIn = (data)=> api.post('/api/signin', data);
 export const addPhoto = (data)=> api.post('/api/addphoto', data);
+export const getRooms = ()=> api.get('/api/getrooms');
 export default api;
