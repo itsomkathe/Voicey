9aca1b17dbf0fe5581b024ffc4eee17f7c32e7c5
Om Kathe <omkathe26@gmail.com>
2021-12-17 18:16:19 +0530

    feat: room-card-real-time

--- frontend/src/Components/Common/RoomCard/RoomCard.jsx
+++ frontend/src/Components/Common/RoomCard/RoomCard.jsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import style from './RoomCard.module.css';
 import { useHistory } from 'react-router-dom';
-
+const backend = process.env.REACT_APP_BACKEND_URL;
 const RoomCard = ({ room }) => {
     const history = useHistory();
     return (
         <div
             onClick={() => {
-                history.push(`/room/${room.id}`);
+                history.push(`/room/${room._id}`);
             }}
             className={style.card}
         >
@@ -20,15 +20,15 @@ const RoomCard = ({ room }) => {
                 <div className={style.avatars}>
                     {room.speakers.map((speaker) => (
                         <img
-                            key={speaker.id}
-                            src={speaker.avatar}
+                            key={speaker._id}
+                            src={backend + speaker.picture}
                             alt="speaker-avatar"
                         />
                     ))}
                 </div>
                 <div className={style.names}>
                     {room.speakers.map((speaker) => (
-                        <div key={speaker.id} className={style.nameWrapper}>
+                        <div key={speaker._id} className={style.nameWrapper}>
                             <span>{speaker.name}</span>
                             <img
                                 src="/Resources/Icons/online-dot.png"
