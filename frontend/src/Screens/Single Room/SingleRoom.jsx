2ebf98fdf3862af62066abad0b72c616289f5d15
Om Kathe <omkathe26@gmail.com>
2022-01-24 10:09:00 +0530

    feat: set-audio-instance

--- frontend/src/Screens/Single Room/SingleRoom.jsx
+++ frontend/src/Screens/Single Room/SingleRoom.jsx
@@ -1,25 +1,26 @@
 import React, { useEffect, useState } from "react";
+import { useSelector } from "react-redux";
 import { useParams } from "react-router-dom";
+import { useMedia } from "../../CustomHooks/useMedia";
 import style from "./SingleRoom.module.css";
 
 export default function SingleRoom() {
     const { roomID } = useParams();
-    const [clients, setClients] = useState([
-        {
-            id: 1,
-            name: "Venugopal Iyer",
-        },
-        {
-            id: 2,
-            name: "Sean Paul",
-        },
-    ]);
+    const user = useSelector((state)=>state.profile);
+    const [clients, provideRef] = useMedia(roomID, user);
+
+    
     return (
         <>
             {clients.map((client) => {
                 return (
-                    <div key={client.id}>
-                        <audio controls autoPlay></audio>
+                    <div key={client._id}>
+                        <audio 
+                            ref = {(instance)=>{
+                                provideRef(instance, client._id)
+                            }}
+                            controls 
+                            autoPlay></audio>
                         <h4>{client.name}</h4>
                     </div>
                 );
