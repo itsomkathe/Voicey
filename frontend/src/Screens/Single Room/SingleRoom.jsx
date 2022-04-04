2114cda12bc9506a2b3e94c5844be37da7be4842
Om Kathe <omkathe26@gmail.com>
2022-04-04 15:59:38 +0530

    feat: mute-ui

--- frontend/src/Screens/Single Room/SingleRoom.jsx
+++ frontend/src/Screens/Single Room/SingleRoom.jsx
@@ -10,31 +10,41 @@ export default function SingleRoom() {
     const history = useHistory();
     const { roomId } = useParams();
     const user = useSelector((state) => state.profile);
-    const { clients, provideRef } = useMedia(roomId, user);
+    const { clients, provideRef, handleMute } = useMedia(roomId, user);
+    const [isMute, setMute] = useState(true);
     const [room, setRoom] = useState(null);
-
     const handleManualLeave = () => {
         history.push("/rooms");
     };
+    
+    useEffect(() => {
+        handleMute(isMute, user.id);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [isMute]);
 
-    useEffect(()=>{
-        const fetchRoom = async ()=>{
+    useEffect(() => {
+        const fetchRoom = async () => {
             const response = await getRoom(roomId);
             setRoom(response.data);
-        }
-        try{
+        };
+        try {
             fetchRoom();
-        }catch(err){
+        } catch (err) {
             console.log(err);
         }
-    }, [roomId])
+    }, [roomId]);
+
+    const handleMuteClick = (clientId) => {
+        if(clientId !== user.id){
+            return;
+        }
+        setMute((prev) => !prev);
+    };
     return (
         <>
             <div className={style.clientWrapper}>
                 <div className={style.header}>
-                    <h2 className={style.topic}>
-                        {room ? room.topic : null}
-                    </h2>
+                    <h2 className={style.topic}>{room ? room.topic : null}</h2>
                     <div className={style.actions}>
                         <button title="Wave hand!" className={style.waveButton}>
                             <img
@@ -55,19 +65,33 @@ export default function SingleRoom() {
                             <div key={client.id} className={style.client}>
                                 <div className={style.userHead}>
                                     <audio
-                                    ref={(instance) =>
-                                        provideRef(instance, client.id)
-                                    }
-                                    controls={true}
-                                    autoPlay={true}
-                                ></audio>
+                                        ref={(instance) =>
+                                            provideRef(instance, client.id)
+                                        }
+                                        autoPlay={true}
+                                    ></audio>
                                     <img
                                         className={style.userPicture}
                                         src={client.picture}
                                         alt="userpic"
                                     ></img>
-                                    <button className={style.micButton}>
-                                        <img src="/Resources/Icons/mic-mute.png" alt="micicon"></img>
+                                    <button
+                                        onClick={() =>
+                                            handleMuteClick(client.id)
+                                        }
+                                        className={style.micButton}
+                                    >
+                                        {client.muted ? (
+                                            <img
+                                                src="/Resources/Icons/mic-mute.png"
+                                                alt="mic"
+                                            />
+                                        ) : (
+                                            <img
+                                                src="/Resources/Icons/mic.png"
+                                                alt="mic"
+                                            />
+                                        )}
                                     </button>
                                 </div>
                                 <h5>{client.name}</h5>
